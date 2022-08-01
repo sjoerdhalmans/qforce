1070cf34910db0ba38fe59c3fff33ae748cca036
Sjoerd_Halmans <sjoerd.halmans@gmail.com>
2022-08-01 12:15:46 +0200

    Generated project

---
+++ qforce_Sjoerd/src/main/java/com/qforce/qforce_Sjoerd/QforceSjoerdApplication.java
@@ -0,0 +1,13 @@
+package com.qforce.qforce_Sjoerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QforceSjoerdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QforceSjoerdApplication.class, args);
+	}
+
+}
---
+++ qforce_Sjoerd/src/test/java/com/qforce/qforce_Sjoerd/QforceSjoerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qforce.qforce_Sjoerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QforceSjoerdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
